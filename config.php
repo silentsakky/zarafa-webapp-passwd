ceebc4174e6f67998dc4ec1d06a425248bedb06c
Christoph Haas <christoph.h@sprinternet.at>
2015-05-20 01:34:29 +0200

    LDAP bugfixes, allow non anonymous binds and ssl/tls

--- config.php
+++ config.php
@@ -9,7 +9,16 @@ define('PLUGIN_PASSWD_LDAP', false);
 define('PLUGIN_PASSWD_LDAP_BASEDN', 'dc=example,dc=com');
 
 /** URI to access LDAP server **/
-define('PLUGIN_PASSWD_LDAP_URI', 'localhost');
+define('PLUGIN_PASSWD_LDAP_URI', 'ldap://localhost');
+
+/** Use TLS to access LDAP server **/
+define('PLUGIN_PASSWD_LDAP_USE_TLS', true);
+
+/** Bind DN to access LDAP server (keep empty for anonymous bind) **/
+define('PLUGIN_PASSWD_LDAP_BIND_DN', "");
+
+/** Bind password to access LDAP server (keep empty for anonymous bind) **/
+define('PLUGIN_PASSWD_LDAP_BIND_PW', "");
 
 /** Set to true if you login with username@tenantname **/
 define('PLUGIN_PASSWD_LOGIN_WITH_TENANT', false);
